866614956a32c3f6387e9dd9e24e7f8d2b66aa04
INGANZAMARUMPU <jonkur96@gmail.com>
2022-10-08 09:31:51 +0200

    topbar

--- src/components/topbar.vue
+++ src/components/topbar.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="component">
     <h1>ANT ROBOT COMMANDER</h1>
-    <div>
-      
+    <div class="options">
+      <h1>+</h1>
+      <h1>☼</h1>
     </div>
   </div>
 </template>
@@ -21,5 +22,24 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+.options{
+  display: flex;
+  height: 100%;
+}
+.options > *{
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+}
+.options > *:hover{
+  background-color: var(--primary);
+  color: white;
+  cursor: default;
+}
+.options > *:active{
+  background-color: var(--primary-light);
+  color: white;
+  cursor: default;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,6 @@
       
     </div>
     <div class="commander">
-      
     </div>
   </div>
 </template>

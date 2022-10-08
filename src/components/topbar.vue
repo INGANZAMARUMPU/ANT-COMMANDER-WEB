d23751132997dccd9b65838a1fa17a01f24d79d2
INGANZAMARUMPU <jonkur96@gmail.com>
2022-10-08 09:20:30 +0200

    basic template

---
+++ src/components/topbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="component">
+    <h1>ANT ROBOT COMMANDER</h1>
+    <div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+<style scoped>
+.component{
+  height: 60px;
+  box-shadow: 0 0 5px;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <TopBar/>
+    <div class="terminal">
+      
+    </div>
+    <div class="commander">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import TopBar from '@/components/topbar.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    TopBar
+  }
+}
+</script>

c480b18e9fd0529b3515af71f8deb27125574456
INGANZAMARUMPU <jonkur96@gmail.com>
2023-01-26 14:51:05 +0200

    camera part design

---
+++ src/components/robot.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="robot" @click="changeRobot">
+    <label>{{ name }}</label>
+    <div class="value">{{ id }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    name:{type:String, required:true},
+    id:{type:String, required:true},
+  },
+  data(){
+    return {
+      audios:[]
+    }
+  },
+  methods:{
+    play(){
+      let data = {
+        "sender" : this.$store.state.id,
+        "order" : "dtmf",
+        "message" : this.id
+      }
+      this.$store.state.socket.send(JSON.stringify(data))
+    }
+  }
+}
+</script>
+<style scoped>
+.robot{
+  background-color: var(--primary);
+  padding: 5px;
+  opacity: .5;
+  cursor: default;
+}
+.robot label{
+  font-weight: 800;
+  font-size: 1.1em;
+}
+.value{
+  font-size: 0.8em!important;
+}
+</style>
--- src/views/Commander.vue
+++ src/views/Commander.vue
@@ -2,6 +2,23 @@
   <div id="app">
     <TopBar @close="close"/>
     <div class="body">
+      <div class="camera">
+        <div class="top">
+          <div class="title">{{ selected.name }}</div>
+          <div v-if="!!msg">
+            <div>new command: <strong>{{ msg }}</strong></div>
+          </div>
+        </div>
+        <div class="robots">
+          <Robot
+            class="robot" v-for="ant in robots"
+            :name="ant.name"
+            :id="ant.id"/>
+        </div>
+      </div>
+      <div class="buttons">
+        
+      </div>
     </div>
   </div>
 </template>
@@ -9,13 +26,27 @@
 <script>
 import TopBar from '@/components/topbar.vue'
 import Button from '@/components/button.vue'
+import Robot from '@/components/robot.vue'
 
 export default {
   components: {
-    TopBar, Button
+    TopBar, Button, Robot
   },
   data(){
     return {
+      selected:{
+        name: "Main Ant",
+        id:16153153
+      },
+      robots:[
+        {id:16153153,name:"Ant no. 53"},
+        {id:16153154,name:"Ant no. 54"},
+        {id:16153155,name:"Ant no. 55"},
+        {id:16153156,name:"Ant no. 56"},
+        {id:16153157,name:"Ant no. 57"},
+        {id:16153158,name:"Ant no. 58"},
+        {id:16153159,name:"Ant no. 59"},
+      ]
     }
   },
   methods:{
@@ -23,4 +54,28 @@ export default {
 }
 </script>
 <style scoped>
+.body{
+  display: flex;
+}
+.camera{
+  background-color: black;
+  color: white;
+  padding: 20px;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+}
+.top{
+  flex: 1;
+}
+.title{
+  font-size: 1.2em;
+  font-weight: 800;
+}
+.robots{
+  display: flex;
+  flex-direction: row-reverse;
+  flex-wrap: wrap-reverse;
+  gap: 5px;
+}
 </style>
